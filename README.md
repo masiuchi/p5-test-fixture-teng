e8bac4902fa2d9eb31b121d79930244e9cc1eb3b
Masahiro IUCHI <masahiro.iuchi@gmail.com>
2021-07-25 05:37:52 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Fixture::Teng - It's new $module
+
+# SYNOPSIS
+
+    use Test::Fixture::Teng;
+
+# DESCRIPTION
+
+Test::Fixture::Teng is ...
+
+# LICENSE
+
+Copyright (C) Masahiro IUCHI.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masahiro IUCHI <masahiro.iuchi@gmail.com>
---
+++ lib/Test/Fixture/Teng.pm
@@ -0,0 +1,39 @@
+package Test::Fixture::Teng;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Fixture::Teng - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Fixture::Teng;
+
+=head1 DESCRIPTION
+
+Test::Fixture::Teng is ...
+
+=head1 LICENSE
+
+Copyright (C) Masahiro IUCHI.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro IUCHI E<lt>masahiro.iuchi@gmail.comE<gt>
+
+=cut
+
